e70932efce4651a82d67bb6c637bb4bc8f1a26de
devinsigley <devinsigley@gmail.com>
2019-04-29 17:04:35 -0400

    Added graph support for points.

--- main.cpp
+++ main.cpp
@@ -15,7 +15,8 @@ bool DEBUG = false;
 std::string createLineString(float x1, float y1, float x2, float y2, std::string color, int lineWeight);
 std::string createWall(Wall wall);
 std::string createTarget(int distance);
-std::string createProjectileString(float angleRads, float initialVelocity);
+std::string createPoint(int x, int y);
+std::string createProjectileString(float angleRads, int targetDistance);
 
 int main(int argc, char** argv) {
 
@@ -32,7 +33,7 @@ int main(int argc, char** argv) {
     Wall userWall;
     userWall = inputHandler.getWallDimensions(TARGET_DISTANCE);
     Gnuplot userPlot("tee user.gp | gnuplot -persist");
-    userPlot << createTarget(TARGET_DISTANCE) << createWall(userWall) << createProjectileString(angleForDistance(TARGET_DISTANCE), INITIAL_VELOCITY) << "\n";
+    userPlot  << createPoint(10, 20) << createTarget(TARGET_DISTANCE) << createWall(userWall) << createProjectileString(angleForDistance(TARGET_DISTANCE), TARGET_DISTANCE) << "\n";
 
     return 0;
 }
@@ -53,30 +54,37 @@ std::string createWall(Wall wall){
 
 
 std::string createTarget(int distance){
-    /*
-    os << "set arrow from " << distance-1.5 << "," << 0
-       << " to " << distance+1.5 << "," << 0
-       << " nohead lc rgb \"red\" lw 4;";
-    return os.str();
-     */
     return createLineString(distance-1.5, 0, distance+1.5, 0, "red", 4);
 }
 
 
-// Returns a string which commands gnuplot to draw a projectile curve
-std::string createProjectileString(float angleRads, float initialVelocity){
+std::string createPoint(int x, int y){
     std::ostringstream os;
-    float distance = pow(initialVelocity, 2.0) * sin(2 * angleRads) / GRAVITY;  // calculates total horizontal distance
-    float height = pow(initialVelocity, 2.0) * pow(sin(angleRads), 2.0) / (2 * GRAVITY);    // calculates peak height
+    os << "set label at " << x << ", " << y << "\"\""
+       << " point pointtype 7;";
+    return os.str();
+}
 
-    distance *= 1.1; // have the graph window 10% larger than necessary
-    height *= 1.1;   // have the graph window 10% larger than necessary
+
+// Returns a string which commands gnuplot to draw a projectile curve
+std::string createProjectileString(float angleRads, int targetDistance){
+    std::ostringstream os;
+    float distance = pow(INITIAL_VELOCITY, 2.0) * sin(2 * angleRads) / GRAVITY;  // calculates total horizontal distance
+    float height = pow(INITIAL_VELOCITY, 2.0) * pow(sin(angleRads), 2.0) / (2 * GRAVITY);    // calculates peak height
+
+//    if (distance <= 20){ distance = 20; }
+//    distance *= 1.1; // have the graph window 10% larger than necessary
+//    height *= 1.1;   // have the graph window 10% larger than necessary
+
+    if (targetDistance <= 25) {distance = 30;}
+    else {distance = 100;}
+    height = 50;
 
 
     os << "plot [0:" << distance << "][0:" << height << "] "
        << "tan(" << angleRads << ")*x -("
        << GRAVITY << "*x**2)/(2*"
-       << initialVelocity << "**2*(cos("
+       << INITIAL_VELOCITY << "**2*(cos("
        << angleRads << "))**2)"
        << " tit 'projectile'";
 
