2a757b3188be2d1f4f58b1ab8e2b5cca8881ff5e
devinsigley <devinsigley@gmail.com>
2019-05-01 15:22:37 -0400

    Fixed missing semicolons.

--- ProjCalculations.h
+++ ProjCalculations.h
@@ -23,20 +23,20 @@ float angleForDistance(float distance);
 float timeAtHeight(float height, float angleRads);
 
 // Given a time and angle, returns the horizontal distance of the projectile at that time
-float distanceAtTime(float time, float angleRads)
+float distanceAtTime(float time, float angleRads);
 
 // Given an x-coord, returns the corresponding y-coord
-float heightAtDistance(float distance, float angleRads)
+float heightAtDistance(float distance, float angleRads);
 
 // Given an angle, returns total time of flight
 float timeOfFlight(float angleRads);
 
 // Returns true if projectile hits the wall.
 // Stores potential impact location in coords.
-bool checkWallImpact(float coords[2], float angleRads, float wallDistance, float wallHeight)
+bool checkWallImpact(float coords[2], float angleRads, float wallDistance, float wallHeight);
 
 // Computes how far the projectile is from the goal upon its first collision.
 // Projectile can either stop upon hitting a wall or hitting the ground.
-float distanceFromGoal(float angleRads, float wallDistance, float wallHeight, float targetDistance)
+float distanceFromGoal(float angleRads, float wallDistance, float wallHeight, float targetDistance);
 
 #endif //AIFINAL_PROJCALCULATIONS_H
